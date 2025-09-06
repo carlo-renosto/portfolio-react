9c67f87df04d6adbad8d26fa411ae0b356bdfb11
carlo-renosto <carlorenosto13@gmail.com>
2025-09-06 15:40:53 -0300

    Modificar vite-config.ts para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio-react/',
   server: {
     host: '0.0.0.0', // Accesible desde todos los dispositivos de una misma conexión
     port: 5173,
