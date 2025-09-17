4409bf8bbd61df0f533773f03f88dfdc83328beb
carlo-renosto <carlorenosto13@gmail.com>
2025-09-17 13:02:39 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/portfolio-react/',
+  base: command === 'build' ? '/portfolio-react/' : '/portfolio-react/',
   server: {
-    host: '0.0.0.0', // Accesible desde todos los dispositivos de una misma conexión
+    host: '0.0.0.0', // Accesible desde todos los dispositivos en la misma red
     port: 5173,
     proxy: {
       '/users': {
-      target: 'https://api.github.com',
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, ''),
+        target: 'https://api.github.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
-})
+}))
