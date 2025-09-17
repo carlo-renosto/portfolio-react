d6104244f2f4fc34552fd353b531bd79fccc329a
carlo-renosto <carlorenosto13@gmail.com>
2025-09-17 13:19:10 -0300

    Actualizar archivos de configuración

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   server: {
     host: '0.0.0.0', // Accesible desde todos los dispositivos de una misma conexión
     port: 5173,
